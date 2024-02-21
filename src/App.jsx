d8c6572480e10622d835a95d056f809beba4ef60
muhamash <ashraful@xpeedlab.tech>
2024-02-22 01:37:32 +0600

    project set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-xl text-green-800">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
